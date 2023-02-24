392a02024c60974b0cdf05af44bb9f54897e0042
lgelashvili <lgelashvili@crocobet.com>
2023-02-24 20:18:19 +0400

    add error and success paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/messages/error/error.component';
+import { SuccessComponent } from './components/messages/success/success.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserPostsComponent } from './components/user-posts/user-posts.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
-
   { path: 'user/:id/posts', component: UserPostsComponent },
+  { path: 'success', component: SuccessComponent },
+  { path: 'error', component: ErrorComponent },
 ];
 
 @NgModule({
