d1ea2c9d4d9e41162bb484beaad3cfe7891f9041
lgelashvili <lgelashvili@crocobet.com>
2023-02-24 22:05:30 +0400

    add rute for edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './components/messages/error/error.component';
 import { SuccessComponent } from './components/messages/success/success.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserPostsComponent } from './components/user-posts/user-posts.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'user/:id/posts', component: UserPostsComponent },
+  { path: 'user/:id/:post/edit', component: PostEditComponent },
   { path: 'success', component: SuccessComponent },
   { path: 'error', component: ErrorComponent },
 ];
