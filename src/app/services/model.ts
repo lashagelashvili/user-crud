b60718a4a29f4808ef87e29da3cd9a302c488749
lgelashvili <lgelashvili@crocobet.com>
2023-02-24 19:03:21 +0400

    add user interface

---
+++ src/app/services/model.ts
@@ -0,0 +1,34 @@
+export interface User {
+  id: number;
+  emai: string;
+  name: string;
+  phone: string;
+  username: string;
+  website: string;
+  address: Adress;
+  company: Company;
+}
+
+interface Adress {
+  city: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+  street: string;
+  suite: string;
+  zipcode: string;
+}
+
+interface Company {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+}
+
+export interface UserPosts {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
