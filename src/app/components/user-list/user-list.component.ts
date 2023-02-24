265ecb254f2c65401f5150c6496889aa800796cf
lgelashvili <lgelashvili@crocobet.com>
2023-02-24 22:35:24 +0400

    cleanup

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,15 +7,13 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnInit {
-  users$ = this.userService.getUsers().pipe(tap(console.log));
+export class UserListComponent {
+  users$ = this.userService.getUsers();
 
   constructor(
     private readonly userService: UserService,
     private readonly router: Router
   ) {}
-
-  ngOnInit(): void {}
 
   onUserClick(id: number) {
     this.router.navigateByUrl(`user/${id}/posts`);
