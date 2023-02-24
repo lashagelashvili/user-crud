fb990d4eca22f47b139ad72346b9faae6629ceca
lgelashvili <lgelashvili@crocobet.com>
2023-02-24 22:09:04 +0400

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserPostsComponent } from './components/user-posts/user-posts.component';
+import { SuccessComponent } from './components/messages/success/success.component';
+import { ErrorComponent } from './components/messages/error/error.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent, UserPostsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    UserPostsComponent,
+    SuccessComponent,
+    ErrorComponent,
+    PostEditComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
